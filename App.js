535d4fafacad36862b3e561e62d867a5fef79f4a
Aashna1345 <94697485+Aashna1345@users.noreply.github.com>
2023-10-19 16:11:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import ChatRoom from './Component/ChatRoom';
+import TaskBoard from './Component/TaskBoard';
+
+function App() {
+  return (
+    <div className="App">
+          <ChatRoom />
+          <TaskBoard />
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// MongoDB connection
-mongoose.connect('mongodb://localhost/mern_chat_task_board', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-
-// Socket.io configuration
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle real-time chat and task board updates here
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
